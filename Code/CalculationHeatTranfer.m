01b33bb5509a95b58fea035e7d9952b5eb1608c7
NutthapoomN <nnutthapoom@gmail.com>
2026-02-16 23:51:54 +0700

    Update Juned Air by Temp Simulink fix 2 point

--- Code/CalculationHeatTranfer.m
+++
@@ -1,7 +0,0 @@
-%Calculation Heat transfer THermal Storage
-
-%% Tube
-DiameterInside = 100*10^-3 ;
-DiameterOutside= (6+100)*10^-3;
-
-900*10/(900-200)
--- clean_dataa.m
+++
@@ -1,1 +0,0 @@
-T = readtable('D:\Senior Project\Excel\20_1_2026.xlsx');

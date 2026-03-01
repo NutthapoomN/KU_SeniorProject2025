4630620d2c1155d890f6364dff899d685c8386aa
NutthapoomN <nnutthapoom@gmail.com>
2026-03-01 17:29:04 +0700

    update tes

---
+++ Matlab&Simulink/slprj/_cgxe/untitled/src/untitled_cgxe.c
@@ -0,0 +1,18 @@
+/* Include files */
+
+#include "untitled_cgxe.h"
+#include "m_hboOEvACJQG1RbnxdXwlKG.h"
+
+unsigned int cgxe_untitled_method_dispatcher(SimStruct* S, int_T method, void
+  * data)
+{
+  if (ssGetChecksum0(S) == 2253897685 &&
+      ssGetChecksum1(S) == 1417043998 &&
+      ssGetChecksum2(S) == 2831984879 &&
+      ssGetChecksum3(S) == 3805968937) {
+    method_dispatcher_hboOEvACJQG1RbnxdXwlKG(S, method, data);
+    return 1;
+  }
+
+  return 0;
+}
